6365250dc442b91ac6f72f660a5ffff0b375ff3f
atiozn <atifoezen@googlemail.com>
2024-12-28 00:22:44 +0100

    Added player generation and display

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import * 
+from player import *
 
 def main():
 	pygame.init()
@@ -11,6 +12,8 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 	clock = pygame.time.Clock()
 	dt = 0
 	while True:
@@ -18,6 +21,7 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
 
